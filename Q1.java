46f6572bc87e4877dcc1de623c0a4a912aa52574
RaeesAhmedh <139773938+RaeesAhmedh@users.noreply.github.com>
2023-07-17 19:41:53 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,7 @@
+
+public class Q1 {
+
+    public static void main(String args[]) {
+        System.out.println(" I am a student of Uva Wellassa University.\n My department is \"Computer Science &\n Infomatics\".");
+    }
+}
